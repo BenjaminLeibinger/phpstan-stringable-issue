2a0369325b586bc6a3b4b832ddf755a56c332a61
Benjamin Leibinger <benjamin.leibinger@burda.com>
2021-11-24 10:41:13 +0100

    cleanup Validator

--- src/Utils/Validator.php
+++ src/Utils/Validator.php
@@ -22,29 +22,10 @@ use function Symfony\Component\String\u;
  */
 class Validator
 {
-    public function validatePassword(?string $plainPassword): string
+    public function validatePassword(?string $plainPassword): ?string
     {
-        if (empty($plainPassword)) {
-            throw new InvalidArgumentException('The password can not be empty.');
-        }
-
-        if (u($plainPassword)->trim()->length() < 6) {
-            throw new InvalidArgumentException('The password must be at least 6 characters long.');
-        }
+        $passwordLength = u($plainPassword)->trim()->length();
 
         return $plainPassword;
-    }
-
-    public function validateEmail(?string $email): string
-    {
-        if (empty($email)) {
-            throw new InvalidArgumentException('The email can not be empty.');
-        }
-
-        if (null === u($email)->indexOf('@')) {
-            throw new InvalidArgumentException('The email should look like a real email.');
-        }
-
-        return $email;
     }
 }
